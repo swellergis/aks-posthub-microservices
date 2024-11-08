5949088c9b6372b11faad84c89a2a98d99f6a43f
swellergis <shaun.weller@gmail.com>
2024-11-08 07:45:39 -0700

    modify nats url

--- ui/src/pages/_app.js
+++ ui/src/pages/_app.js
@@ -12,9 +12,48 @@ const uiURL = process.env.NEXT_PUBLIC_SITE_URL;
 const MyApp = ({ Component, pageProps, loggedInUser }) => {
   return (
     <div>
-      <p>_app.js</p>
-      </div>
+      <Head>
+        <title>Posthub, Enabing Borderless Conversations</title>
+        <meta
+          name="description"
+          content="Discuss and share thoughts or ideas"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header loggedInUser={loggedInUser} />
+      <Component loggedInUser={loggedInUser} {...pageProps} />
+
+      <footer className={styles.footer}>
+        <a
+          href={uiURL}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by Posthub{" "}
+          <span className={styles.logo}>
+          </span>
+        </a>
+      </footer>
+    </div>
   );
+};
+
+MyApp.getInitialProps = async (myAppContext) => {
+  const backendClient = axiosSrv(myAppContext.ctx);
+  const { data } = await backendClient.get("/api/users/activeuser");
+
+  const pageProps = myAppContext.Component.getInitialProps
+    ? await myAppContext.Component.getInitialProps(
+        myAppContext.ctx,
+        backendClient,
+        data.loggedInUser
+      )
+    : {};
+
+  return {
+    pageProps,
+    ...data,
+  };
 };
 
 export default MyApp;
